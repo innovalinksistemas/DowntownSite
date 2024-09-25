845821353bd142138554ab4be18fdb20f1da053e
Wil <sevillawil8@gmail.com>
2024-09-24 20:50:19 -0600

    Creacion de carpetas y componentes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,20 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				rojo: '#6A28331',
+				azulOscuro: '#424B59',
+				fondo: '#FFF5E4'
+			},
+
+			fontFamily: {
+				BadScript: ["Bad Script", "sans-serif"],
+				Lobster: ["Lobster", "sans-serif"],
+				Montserrat: ["Montserrat", "sans-serif"],
+			  },
+		},
+
 	},
 	plugins: [],
 }
