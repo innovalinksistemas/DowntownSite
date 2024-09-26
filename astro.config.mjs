e20fd5dd2c20a3fe666b5ff617adef6306adabe4
Wil <sevillawil8@gmail.com>
2024-09-26 15:06:33 -0600

    ajuste deploy github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://wdsevilla.github.io',
+  base:'Downtown',
   image: {
     service: passthroughImageService(),
   },
