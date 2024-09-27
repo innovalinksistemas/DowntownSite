4e26596a00a25ae36a8b6a89a2c52fce62f2a4e7
Wil <sevillawil8@gmail.com>
2024-09-27 14:36:05 -0600

    cambio fondos a public

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,8 +7,8 @@ export default {
 	theme: {
 		extend: {
 			backgroundImage:{
-				'fondo1': "url('./src/assets/fondo1.png')",
-				'fondoMovil':"url('./src/assets/room1.jpg')"
+				'fondo1': "url('./public/fondo1.png')",
+				'fondoMovil':"url('./public/room1.jpg')"
 			},
 			colors:{
 				rojo: '#6A28331',
