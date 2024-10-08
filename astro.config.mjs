403bd82d59d30407394f865b5502c4e87c3c9b10
Wil <sevillawil8@gmail.com>
2024-10-07 23:33:54 -0600

    se agrega el site para el build de github

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://wdsevilla.github.io',  
+  site: 'https://wdsevilla.github.io',
+base:'Downtown',  
   image: {
     service: passthroughImageService(),
   },
