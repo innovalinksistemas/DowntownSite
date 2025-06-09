f36353a92844379ed60ea3600b02166fe4a2462c
Sistemas <sistemas@innovalinkcr.com>
2025-06-09 14:02:08 -0600

    update package

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,19 +4,22 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-
   i18n:{
     defaultLocale: "es",
     locales: ["es","en"],
   },
 
   site: 'https://wdsevilla.github.io',
-base:'/Downtown/',  
+  base:'/Downtown/',
+
   image: {
     service: passthroughImageService(),
   },
 
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  adapter: netlify()
 });
